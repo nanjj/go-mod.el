dd93413b4f827c65d3cdab3b044e0e28bbf435f0
JUN JIE NAN <nanjunjie@gmail.com>
2019-03-28 23:37:45 +0800

    Autoloads adjustment

--- go-mod.el
+++ go-mod.el
@@ -14,36 +14,37 @@
 ;;
 ;;; Code:
 
+;;;###autoload
 (defun go-mod-tidy()
   "Run go mod tidy"
   (interactive)
   (shell-command "go mod tidy -v"))
 
+;;;###autoload
 (defun go-mod-graph()
   "Run go mod graph"
   (interactive)
   (shell-command "go mod graph"))
 
+;;;###autoload
 (defun go-mod-init()
   "Run go mod init"
   (interactive)
   (shell-command "go mod init"))
 
+;;;###autoload
 (defun go-mod-verify()
   "Run go mod verify"
   (interactive)
   (shell-command "go mod verify"))
 
-;;;###autoload
 (defconst go-mod-mode-syntax-table
-  (-let [table
-         (copy-syntax-table prog-mode-syntax-table)]
+  (let ((table (make-syntax-table)))
     (modify-syntax-entry ?\/ ". 12b" table)
     (modify-syntax-entry ?\n "> b" table)
     table)
   "go mod mode syntax table.")
 
-;;;###autoload
 (setq go-mod-mode-highlights
       '(("^\\(module\\) " . font-lock-type-face)
         ("^\\(require\\)" . font-lock-function-name-face)
